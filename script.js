66ab20796b12307d0b87530f28715bbe7b9ad0c9
Emily Xie <emilyxxie@gmail.com>
2017-02-25 16:31:51 -0500

    form styled and looking good

--- script.js
+++ script.js
@@ -1,34 +1,48 @@
 $( document ).ready( function() {
 
   var input = $();
-  $('.symptoms_form').submit(function() {
-    console.log("HELLO");
+
+
+  $('.symptoms_button').click(function() {
     var symptoms_input = $("input").val();
+    if (symptoms_input == "") {
+      return;
+    }
+    // var symptoms_input = $()
     var symptoms_array = symptoms_input.split(",");
     for (var i = 0; i < symptoms_array.length; i++) {
       symptoms_array[i] = symptoms_array[i].trim().toLowerCase();
       // TODO: get rid of last comma, and empty string
     }
 
+    // TODO: if the disease summary has the words "fatal" in it, add more points to "count"
     var diseases_hash = {};
     for (var disease in diseases) {
       var disease_symptoms = diseases[disease]['symptoms'];
-      // for (var i = 0; i < disease_symptoms.length; i ++) {
       disease_symptoms.forEach(function(disease_symptom) {
         symptoms_array.forEach(function(user_symptom) {
           if (disease_symptom.trim().toLowerCase() == user_symptom) {
             if (disease in diseases_hash) {
               diseases_hash[disease]['count'] += 1;
+              diseases_hash[disease]['symptoms'].push(user_symptom);
+              // TODO: display the symptoms matched
             } else {
               diseases_hash[disease] = {'count': 1};
+              diseases_hash[disease]['symptoms'] = [user_symptom];
+              diseases_hash[disease]['summary'] = diseases[disease]['summary'];
+              diseases_hash[disease]['url'] = diseases[disease]['url'];
+              diseases_hash[disease]['all_symptoms'] = diseases[disease]['all_symptoms'];
             }
           }
         });
       });
     }
 
+    $('.symptoms_form').fadeOut(300);
 
-    console.log(diseases_hash);
+
   });
+
+
 
 });
