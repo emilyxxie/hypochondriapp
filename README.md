f5bbe06321e957ca91a915af9bb6038af504a814
Emily Xie <emilyxxie@gmail.com>
2017-02-25 01:52:32 -0500

    Disease scraper written, about to run it.

---
+++ README.md
@@ -0,0 +1,5 @@
+# hypochondriapp
+
+Enter  your symptoms. Get back the scariest sounding disease possible.
+
+(A remake of an old personal project from 2013)
--- lib/disease_scraper.py
+++ lib/disease_scraper.py
@@ -1,76 +1,119 @@
-import wikipedia
 import json
 import sys
 import re
 import string
-import urllib2
 from bs4 import BeautifulSoup
 import pdb
-
-# rare_diseases = "https://en.wikipedia.org/w/index.php?title=Category:Rare_diseases"
-
+import urllib.request
+from urllib import parse
 
 page_name = "https://en.wikipedia.org/w/index.php?title=Category:Rare_diseases&from="
-# page_name = ""
-links = set()
+diseases = set()
+
+
+################################## MAIN LOOP ##################################
+
 
 for c in string.ascii_lowercase:
-  opener = urllib2.build_opener()
+  opener = urllib.request.build_opener()
   opener.addheaders = [('User-agent', 'Mozilla/5.0')] #wikipedia needs this
 
   resource = opener.open(page_name + c)
-
-  print(page_name + c)
 
   data = resource.read()
   resource.close()
   soup = BeautifulSoup(data, "html.parser")
 
-  links |= set(map(lambda t: t.get("href")[6:].encode(sys.getdefaultencoding()),
+  diseases |= set(map(lambda t: t.get("href")[6:],
                soup.find(id="mw-pages").find(class_="mw-category").find_all("a")))
 
 
-replacers = [('%E2%80%93', '_'), ('%C3%A9', 'e'), ('%C3%B6', 'o')]
-print(len(links))
-for link in links:
-  print 'fetching', link
-  try:
-    for r, s in replacers:
-      link = link.replace(r, s)
-    page = wikipedia.page(link)
-  except wikipedia.exceptions.PageError:
-    print 'bad link:', link
+diseases_batch = 0
+diseases_dict = {}
+for disease in diseases:
 
-  # print(page.content)
+  opener = urllib.request.build_opener()
+  opener.addheaders = [('User-agent', 'Mozilla/5.0')] #wikipedia needs this
 
-# regex = r"<a.*?>(.*?)<\/a>"
-# symptoms = json.loads(open('../symptoms.js', 'r').read())
-# symptoms_regex = ""
-# group_count = 0
+  resource = opener.open("https://en.wikipedia.org/wiki/" + disease)
+  data = resource.read()
+  resource.close()
 
-# symptoms_array = set()
-# complete_symptom_list = set()
+  soup = BeautifulSoup(data, "html.parser")
+  disease_content = soup.find(id="mw-content-text").get_text()
+  disease_summary = re.sub('\[.{1}\]', '', soup.find('p').get_text())
 
-# for symptom in symptoms['symptoms']:
-#   group_count += 1
-#   symptoms_array.add(
-#     symptom.encode(sys.getdefaultencoding())
-#   )
+  regex = r"<a.*?>(.*?)<\/a>"
+  symptoms = json.loads(open('../symptoms.js', 'r').read())
+  symptoms_regex = ""
+  group_count = 0
 
-#   if group_count == 100:
-#     # we must batch the regex match since there is a 100 group count limit
-#     symptoms_array = list(symptoms_array)
-#     symptoms_regex = "|".join(symptoms_array)
-#     sd_matches = re.findall(r'%s' % symptoms_regex, disease.content, re.IGNORECASE)
-
-#     for sd_match in sd_matches:
-#       sd_match = sd_match.encode(sys.getdefaultencoding()).lower()
-#       complete_symptom_list.add(sd_match)
-
-#     symptoms_array = set()
-#     symptoms_regex = ""
-#     group_count = 0
+  symptoms_array = set()
+  complete_symptom_list = set()
 
 
+  for symptom in symptoms['symptoms']:
+    group_count += 1
+    symptoms_array.add(symptom)
 
-# print(complete_symptom_list)
+    if group_count == 100:
+      # we must batch the regex match since there is a 100 group count limit
+      symptoms_array = list(symptoms_array)
+      symptoms_regex = "|".join(symptoms_array)
+      sd_matches = re.findall(r'%s' % symptoms_regex, disease_content, re.IGNORECASE)
+
+      for sd_match in sd_matches:
+        complete_symptom_list.add(sd_match.lower())
+
+      symptoms_array = set()
+      symptoms_regex = ""
+      group_count = 0
+
+  disease_formatted = re.sub('_', ' ', parse.unquote(disease))
+
+  if not complete_symptom_list:
+    print("No symptoms: not adding {} to JSON".format(disease_formatted))
+  else:
+    print("Adding {} to JSON".format(disease_formatted))
+    diseases_dict[disease_formatted] = {
+        'symptoms': list(complete_symptom_list),
+        'summary': disease_summary,
+        'url': "https://en.wikipedia.org/wiki/" + disease
+    }
+
+  diseases_batch += 1
+  if diseases_batch == 300:
+    joined_diseases = ", ".join(diseases_dict.keys())
+    print("Writing to diseases.js: {}".format(joined_diseases))
+    with open("../diseases.js", "a") as diseases_file:
+      json.dump(diseases_dict, diseases_file)
+      diseases_file.write("\n")
+
+    diseases_dict = {}
+    diseases_batch = 0
+
+joined_diseases = ", ".join(diseases_dict.keys())
+print("Final write to diseases.js: {}".format(joined_diseases))
+with open("../diseases.js", "a") as diseases_file:
+  json.dump(diseases_dict, diseases_file, indent=2)
+
+"""
+
+
+{'disease' : {'numbess': null, 'inflammation': null}}
+
+disease_database:
+disease: {name, webpage URL, symptoms}
+
+
+symptoms_diseases.js:
+
+  "symptom": "disease", "disease", "disease",
+
+For every symptom user entered,
+  Get all diseases.
+  Get the count of which disease appears the most
+  Get a weighted calculation of scary + total symptoms.
+
+
+"""
