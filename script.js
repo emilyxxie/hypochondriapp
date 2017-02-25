ed124e2e0b1493a1d36b668f4a2cf39150ef5cfa
Emily Xie <emilyxxie@gmail.com>
2017-02-25 18:09:41 -0500

    Renders the disease. Wow, this code is hilariously bad

--- script.js
+++ script.js
@@ -1,6 +1,71 @@
 $( document ).ready( function() {
 
-  var input = $();
+  // var interstitialWait = 1500;
+  var interstitialWait = 1;
+  var interstitialMedium = 900;
+  var interstitialLong = 3000;
+  var interstitialFast = 550;
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  async function makeSleep() {
+    await sleep(2000);
+    console.log("Sleep");
+
+  }
+
+  async function noDiseases() {
+    await sleep(500);
+    $('.tag_line').text("Analyzing your symptoms...");
+    $('.tag_line').fadeIn(100);
+    await sleep(interstitialWait);
+    $('.tag_line').text("Checking our databases...");
+    await sleep(interstitialWait);
+    $('.tag_line').text("Searching for possible diagnoses...");
+    await sleep(interstitialWait);
+    $('.tag_line').text("Sorry, no diseases match your symptoms.");
+  }
+
+  async function diagnose(disease) {
+    await sleep(500);
+    $('.tag_line').text("Analyzing your symptoms...");
+    $('.tag_line').fadeIn(100);
+    await sleep(interstitialWait);
+    $('.tag_line').text("Checking our databases...");
+    await sleep(interstitialWait);
+    $('.tag_line').text("Searching for possible diagnoses...");
+    await sleep(interstitialWait);
+    $('.tag_line').text("Hmmmm.");
+    await sleep(interstitialWait);
+    $('.tag_line').text("This isn't looking good.");
+    await sleep(interstitialWait);
+    $('.tag_line').text("...");
+    await sleep(interstitialWait);
+    // TODO: vary the screen message.
+    $('.tag_line').css("font-size", "1800%");
+    $('.tag_line').text("OH");
+    await sleep(interstitialFast);
+    $('.tag_line').text("MY");
+    await sleep(interstitialFast);
+    $('.tag_line').text("GOD.");
+    await sleep(interstitialFast);
+    $('.tag_line').text("YOU");
+    await sleep(interstitialFast);
+    $('.tag_line').text("COULD");
+    await sleep(interstitialFast);
+    $('.tag_line').text("HAVE");
+    await sleep(interstitialFast);
+    $('.tag_line').css("font-size", "450%");
+    $('.tag_line').css("font-weight", "800");
+    $('.tag_line').css("color", "rgb(187, 21, 21)");
+    $('.tag_line').text(disease.toUpperCase());
+    await sleep(interstitialLong);
+    $('.tag_line').fadeOut();
+    $('.symptoms_form').remove();
+
+  }
 
 
   $('.symptoms_button').click(function() {
@@ -38,7 +103,22 @@ $( document ).ready( function() {
       });
     }
 
-    $('.symptoms_form').fadeOut(300);
+    $('.tag_line').fadeOut(300);
+    $('.symptoms_input_box').fadeOut(300);
+    $('.symptoms_button').fadeOut(300);
+
+    if (Object.keys(diseases_hash).length == 0) {
+      noDiseases();
+    } else {
+      // var diagnosis;
+      // for (var disease in diseases_hash) {
+      //   if (disease)
+      // }
+      diagnose("Capillary Leak Syndrome with Arial Deficiency Type I");
+    }
+
+
+
 
 
   });
