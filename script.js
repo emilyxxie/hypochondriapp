8cd5504b0172568648dee338bf006fa0efd1bdad
Emily Xie <emilyxxie@gmail.com>
2017-02-25 14:26:35 -0500

    basic algorithm complete

--- script.js
+++ script.js
@@ -5,11 +5,30 @@ $( document ).ready( function() {
     console.log("HELLO");
     var symptoms_input = $("input").val();
     var symptoms_array = symptoms_input.split(",");
-    for (var i = 0; i < symptoms_array.length(); i++) {
-
+    for (var i = 0; i < symptoms_array.length; i++) {
+      symptoms_array[i] = symptoms_array[i].trim().toLowerCase();
+      // TODO: get rid of last comma, and empty string
     }
 
-    console.log(symptoms_array);
+    var diseases_hash = {};
+    for (var disease in diseases) {
+      var disease_symptoms = diseases[disease]['symptoms'];
+      // for (var i = 0; i < disease_symptoms.length; i ++) {
+      disease_symptoms.forEach(function(disease_symptom) {
+        symptoms_array.forEach(function(user_symptom) {
+          if (disease_symptom.trim().toLowerCase() == user_symptom) {
+            if (disease in diseases_hash) {
+              diseases_hash[disease]['count'] += 1;
+            } else {
+              diseases_hash[disease] = {'count': 1};
+            }
+          }
+        });
+      });
+    }
+
+
+    console.log(diseases_hash);
   });
 
 });
